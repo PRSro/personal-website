c22ac06be964aba413895b621f078dc9a03a1f76
PRS <prsro.email@gmail.com>
2026-02-21 21:55:16 +0200

    Save current work before deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/personal-website/',
+  plugins: [react()]
 })
 
 // psCTF{Y0u_F0UnD_Wh1t_1_tw2st24djn32}
